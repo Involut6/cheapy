99f197115dcf80aec915fb6da29218fa0bb76fc6
Abdulqoyum6 <116152272+Abdulqoyum6@users.noreply.github.com>
2023-02-22 12:38:24 +0100

    fixes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -18,7 +18,7 @@ const toggleMenu = () => {
 <template>
     <div class="w-full bg-white h-fit md:px-8 px-4 lg:px-16 flex justify-between items-center border-b">
         <router-link to="/" class="py-2"><img src="../assets/images/logo.svg" /></router-link>
-        <div class="flex space-x-24 items-center">
+        <div class="flex space-x-25 items-center">
             <ul class="decoration-none w-full bg-white md:drop-shadow-none md:static absolute h-fit drop-shadow-md md:w-auto z-10 top-[63px] duration-300 ease-in md:p-0 md:flex md:space-x-8 lg:space-x-16 h-full font-bold" :class="dropDown === true ? 'left-0' : 'left-[-100%]'">
             <router-link active-class="bg-[#0CB5BE] md:text-white" to="/clothing" @click="() => {dropDown = false}"><li class="md:py-[22px] md:px-2 px-4 cursor-pointer py-2">Clothing</li></router-link>
             <router-link active-class="bg-[#0CB5BE] md:text-white" to="/accessories" @click="() => {dropDown = false}"><li class="md:py-[22px] md:px-2 px-4 cursor-pointer py-2">Accessories</li></router-link>
